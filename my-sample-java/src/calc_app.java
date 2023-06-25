efb1663f27f752650ee2974528f5ad31dbed922e
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-06-25 19:22:40 +0300

    First version. There is some KISS, DRY and SRP leaks (main class is too big)

--- my-sample-java/src/calc_app.java
+++ my-sample-java/src/calc_app.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.time.Month;
 import java.util.List;
 import java.nio.file.*;
 
@@ -8,6 +9,37 @@ public class calc_app {
 	public static void main(final String... args) throws IOException {
 		final Path path = Paths.get(RESOURCES + args[0]);
 		final List<String> lines = Files.readAllLines(path);
-	}
+		
+		final List<BankTransaction> bankTransactions = BankStatementCSVParser.parseLinesFromCSV(lines);
+		
+		/*
+		System.out.println("The path is " + path.toString());
+		System.out.println(bankTransactions.getClass());
+		*/
+		//System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+		System.out.println("Transactions in January " + calculateTotalInMonth(bankTransactions,	Month.FEBRUARY));
 
+	}
+	
+	public static double calculateTotalInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+		double total = 0;
+		for(final BankTransaction bankTransaction: bankTransactions) {
+			if(bankTransaction.getDate().getMonth() == month){
+				total += bankTransaction.getAmount();
+			}
+		}
+		return total;
+	}
+	
+	public static double calculateTotalForCategory(final List<BankTransaction> bankTransactions, final String category) {
+		double total = 0;
+		
+		for(final BankTransaction bankTransaction: bankTransactions) {
+			if(bankTransaction.getDescription().equals(category)) {
+				total += bankTransaction.getAmount();
+			}
+		}
+		
+		return total;
+	}
 }
